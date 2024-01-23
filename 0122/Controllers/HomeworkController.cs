41ad83a8cabaa74148dcc8a27ca1f5db5ba75289
snow <snow860408@gmail.com>
2024-01-23 09:15:32 +0800

    加入專案檔案。

---
+++ 0122/Controllers/HomeworkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _0122.Controllers
+{
+	public class HomeworkController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}

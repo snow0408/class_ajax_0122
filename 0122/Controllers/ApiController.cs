0939a6c6602cea3dbb542c2489c73486aebea9cf
snow <snow860408@gmail.com>
2024-01-23 09:15:32 +0800

    加入專案檔案。

---
+++ 0122/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using _0122.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _0122.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDbContext _dbContext;
+        public ApiController(MyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cities()
+        {
+            var cities = _dbContext.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
